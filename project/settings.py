3e9d0ead17a29d54da4ae8ebda5ce37faaa8e39d
abubakar-hamad <abubakar.hmdelneel@gmail.com>
2021-08-24 00:13:30 +0200

    collectstatic

--- project/settings.py
+++ project/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
